f444b419c8cc3b02e43191f7a1a927cf0eda94e8
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-03-30 12:07:05 +0600

    Area and Perimeter of a Rectangle.

---
+++ rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  int num1, num2, result;
+  cout << "Enter the length of Rectangle: ";
+  cin >> num1;
+  cout << "Enter the Width of Rectangle: ";
+  cin >> num2;
+
+  result = num1 * num2;
+
+  cout << "The Area of Ractangle is: " << result << endl;
+
+  return 0;
+}
