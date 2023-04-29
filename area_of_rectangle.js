8f0936e056be83c401bafbce81b93380c5d1a09e
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-29 18:23:19 +0600

    area of rectangle in JavaScript
    area of rectangle in JavaScript

---
+++ area_of_rectangle.js
@@ -0,0 +1,5 @@
+let length = prompt("Enter the Length of Rectangle: ");
+let width = prompt("Enter the Wide if Rectangle: ");
+let area = length * width;
+
+document.write("The area of rectangle is: " + area);
